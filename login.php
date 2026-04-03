9d883596e48ce49faac95a0de6b75cc3ab32a704
Jalu Dwi Bagaskara <jalu.bagaskara@kop.local>
2026-04-03 13:29:01 +0700

    Perbaiki social login di halaman login

--- login.php
+++ login.php
@@ -43,14 +43,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
     <section class="auth bg-base d-flex flex-wrap">
         <div class="auth-left d-lg-block d-none">
             <div class="d-flex align-items-center flex-column h-100 justify-content-center">
-                <img src="assets/images/auth/auth-img.png" alt="">
+                <img src="assets/images/auth/auth-img-2.jpg" alt="">
             </div>
         </div>
         <div class="auth-right py-32 px-24 d-flex flex-column justify-content-center">
             <div class="max-w-464-px mx-auto w-100">
                 <div>
                     <a href="index.php" class="mb-40 max-w-290-px">
-                        <img src="assets/images/logo.png" alt="">
+                        <img src="assets/images/logo-2.png" alt="">
                     </a>
                     <h4 class="mb-12">Sign In to your Account</h4>
                     <p class="mb-32 text-secondary-light text-lg">Welcome back! please enter your detail</p>
@@ -74,7 +74,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
                         </div>
                         <span class="toggle-password ri-eye-line cursor-pointer position-absolute end-0 top-50 translate-middle-y me-16 text-secondary-light" data-toggle="#your-password"></span>
                     </div>
-                    <div class="">
+                    <!-- <div class="">
                         <div class="d-flex justify-content-between gap-2">
                             <div class="form-check style-check d-flex align-items-center">
                                 <input class="form-check-input border border-neutral-300" type="checkbox" value="" id="remeber">
@@ -82,11 +82,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
                             </div>
                             <a href="javascript:void(0)" class="text-primary-600 fw-medium">Forgot Password?</a>
                         </div>
-                    </div>
+                    </div> -->
 
                     <button type="submit" name="login" class="btn btn-primary text-sm btn-sm px-12 py-16 w-100 radius-12 mt-32"> Sign In</button>
 
-                    <div class="mt-32 center-border-horizontal text-center">
+                    <!-- <div class="mt-32 center-border-horizontal text-center">
                         <span class="bg-base z-1 px-4">Or sign in with</span>
                     </div>
                     <div class="mt-32 d-flex align-items-center gap-3">
@@ -101,7 +101,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
                     </div>
                     <div class="mt-32 text-center text-sm">
                         <p class="mb-0">Don’t have an account? <a href="sign-up.php" class="text-primary-600 fw-semibold">Sign Up</a></p>
-                    </div>
+                    </div> -->
 
                 </form>
             </div>
